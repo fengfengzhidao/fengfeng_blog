d14fbc30a34a67dd4abf4222ecc9f56f43fd9fed
fengfeng <2663456523@qq.com>
2022-05-29 11:34:22 +0800

    修改DEBUG

--- Fengfengblog/settings.py
+++ Fengfengblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hy&u3h8i&3#a9y4qvz*^80zak#l-3t1=+%k)hz9u3)#i2dcnu5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
