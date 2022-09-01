e6f68fd7f601ac9682d6d8f6355b0a9a145b6ea1
fengfeng <2663456523@qq.com>
2022-09-01 21:54:41 +0800

    修改新闻api获取

--- api/views/news.py
+++ api/views/news.py
@@ -1,20 +1,26 @@
-# @Time:2022/6/11 20:58
-# @Author:fengfeng
-import requests
+import time
+
 from django.views import View
 from django.http import JsonResponse
+import requests
+# from lib.work_wechat import work
+from lib.get_user_info import get_ip, get_addr_info
 
-url = 'https://api.codelife.cc/api/top/list'
+url = "https://api.codelife.cc/api/top/list"
 
 
 class NewsView(View):
     def post(self, request):
+
         headers = request.headers
+        Signaturekey: str = headers.get('Signaturekey')
         try:
             res = requests.post(url, data=request.data, headers={
-                'signaturekey': headers['Signaturekey'],
-                'version': '1.2.33',
-            })
+                "signaturekey": Signaturekey,
+                "version": '1.2.34',
+                "origin": 'https://go.itab.link',
+                'User-Agent': headers['User-Agent']
+            }, timeout=2).json()
         except Exception:
-            return {'code': 501, 'msg': '请求异常', 'data': []}
-        return JsonResponse(res.json())
+            res = {'code': 501, 'msg': '请求错误', 'data': []}
+        return JsonResponse(res)
